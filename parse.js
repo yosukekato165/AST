0b4a33f91962aec6581a61f10d00c71e3bde491e
yosukekato165 <padyk165@gmail.com>
2021-12-26 13:10:55 +0900

    change easy to read AST

--- parse.js
+++ parse.js
@@ -1,4 +1,5 @@
 const acorn = require("acorn");
+const espurify = require("espurify");
 const code = process.argv[2];
 const ast = acorn.parse(code);
-console.log(ast);
+console.log(JSON.stringify(espurify(ast), null, 2));
